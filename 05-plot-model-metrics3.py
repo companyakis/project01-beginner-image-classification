c21f8952bb62c310538a499c77dff508a9abeb67
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 22:55:24 +0300

    Create 05-plot-model-metrics3.py

---
+++ 05-plot-model-metrics3.py
@@ -0,0 +1,11 @@
+#plotting
+
+df.plot(figsize = (10, 7),
+        xlim = [0, 30],
+        ylim = [0, 1],
+        grid = True,
+        xlabel = "# of Epochs",
+        style = ["b--", "b--*", "r-", "r--."]
+        )
+
+plt.show()
